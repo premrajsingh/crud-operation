6ee6abd93c44754562456e566af1f6ac5f1c2618
Premrajsingh <premrajsingh@Premrajsinghs-MacBook-Air.local>
2025-12-02 00:12:47 +0530

    moved project files to root

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const employeesRouter = require("./routes/employees");
+const employeesRouter = require("./server/routes/employees");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
